4f8a25e5d633155b597a95d96df0778e2cb26528
Nathaniel Lee <nathaniellee@nathaniels-mbp.lan>
2021-05-16 21:20:18 -0700

    duplicate deleted

--- summary_information.R
+++
@@ -1,9 +0,0 @@
-summary_info <- function(ds) {
-  ds_summary <- list()
-  ds_summary$features <- ncol(ds, na.rm = T)
-  ds_summary$observations <- nrow(ds, na.rm = T)
-  ds_summary$male_num <- nrow(filter(ds, SEX == "M"), na.rm = T)
-  ds_summary$female_num <- nrow(filter(ds, SEX == "F"), na.rm = T)
-  ds_summary$avg_age <- round(mean(pull(CAGE_YR)))
-  return(ds_summary)
-}
