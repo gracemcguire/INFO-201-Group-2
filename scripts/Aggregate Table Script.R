b854c9a5d3c08d823d5123986502116660279004
Daniel Choi <USER11@MacBook-Air.local>
2021-05-17 22:34:53 -0700

    got rid of lint errors.

--- scripts/Aggregate Table Script.R
+++ scripts/Aggregate Table Script.R
@@ -1,8 +1,7 @@
-H <- read.csv("2021VAERSDATA.csv", stringsAsFactors = F)
-
+h <- read.csv("2021VAERSDATA.csv", stringsAsFactors = F)
 
 x <- function(SEX, df) {
-  Y <- df %>%
+  y <- df %>%
     group_by(STATE) %>%
     summarise(SEX_male = sum(SEX == "M", na.rm = T),
            SEX_female = sum(SEX == "F", na.rm = T),
@@ -10,15 +9,12 @@ x <- function(SEX, df) {
            AGE_max = max(AGE_YRS, na.rm = T),
            AGE_min = min(AGE_YRS, na.rm = T),
            AGE_avg = min(AGE_YRS, na.rm = T))
-  return(Y)
+  return(y)
 }
 
-h <- x("AGE", H)
+v <- x("AGE", h)
 
 # We included the table so that the columns that we thought were important would
 # be displayed. The information reveals the columns obtaining the SEX, column
 # containing either Male, Female, or Unidentified. We also found the Age group
 # max, min, and mean.
-
-
-
