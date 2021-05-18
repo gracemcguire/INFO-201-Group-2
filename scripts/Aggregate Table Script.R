3321bd3cef8cb183de96c488289e3fa9b53f242a
Daniel Choi <USER11@MacBook-Air.local>
2021-05-17 22:29:45 -0700

    pushing the Aggregate Table Script.

---
+++ scripts/Aggregate Table Script.R
@@ -0,0 +1,24 @@
+H <- read.csv("2021VAERSDATA.csv", stringsAsFactors = F)
+
+
+x <- function(SEX, df) {
+  Y <- df %>%
+    group_by(STATE) %>%
+    summarise(SEX_male = sum(SEX == "M", na.rm = T),
+           SEX_female = sum(SEX == "F", na.rm = T),
+           SEX_unknown = sum(SEX == "U", na.rm = T),
+           AGE_max = max(AGE_YRS, na.rm = T),
+           AGE_min = min(AGE_YRS, na.rm = T),
+           AGE_avg = min(AGE_YRS, na.rm = T))
+  return(Y)
+}
+
+h <- x("AGE", H)
+
+# We included the table so that the columns that we thought were important would
+# be displayed. The information reveals the columns obtaining the SEX, column
+# containing either Male, Female, or Unidentified. We also found the Age group
+# max, min, and mean.
+
+
+
