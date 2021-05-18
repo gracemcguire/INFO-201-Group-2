f3f405e7cafdfee3d11c98bdfab3409e0da40549
Nathaniel Lee <nathaniellee@nathaniels-mbp.lan>
2021-05-17 22:42:32 -0700

    added description

--- scripts/summary_information.R
+++ scripts/summary_information.R
@@ -5,8 +5,11 @@ summary_info <- function(ds) {
   ds_summary$features <- ncol(ds)
   ds_summary$observations <- nrow(ds)
   ds_summary$male_num <- nrow(filter(ds, SEX == "M"))
+  #Number of males who have reported symptoms
   ds_summary$female_num <- nrow(filter(ds, SEX == "F"))
+  #Number of females who have reported symptoms
   ds_summary$avg_age <- round(mean(pull(ds, CAGE_YR), na.rm = TRUE))
+  #Average age
   return(ds_summary)
 }
 
