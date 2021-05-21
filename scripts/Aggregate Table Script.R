44ce8b975c385ce1af77907ada91ba045dee88d0
Daniel Choi <USER11@MacBook-Air.local>
2021-05-20 23:56:34 -0700

    added changes to the table file.

--- scripts/Aggregate Table Script.R
+++ scripts/Aggregate Table Script.R
@@ -7,7 +7,8 @@ aggregate_info <- function(df) {
     summarise(Male = sum(SEX == "M", na.rm = T),
               Female= sum(SEX == "F", na.rm = T),
               Unknown = sum(SEX == "U", na.rm = T),
-              AverageAge = mean(AGE_YRS, na.rm = T))
+              AverageAge = mean(AGE_YRS, na.rm = T)) %>%
+    arrange(-AverageAge)
   colnames(summ) <- c("State", "Males", "Females", "Unknown", "Average Age")
   return(summ)
 }
